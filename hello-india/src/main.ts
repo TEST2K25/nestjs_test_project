10cd2d4a95af301885b2ab6d4b5672b976461189
YAMITRAO <37434328+YAMITRAO@users.noreply.github.com>
2024-12-28 01:55:32 +0530

    test

--- hello-india/src/main.ts
+++ hello-india/src/main.ts
@@ -2,10 +2,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as serverless from 'serverless-http';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
+
+const expressApp = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
   app.enableCors();
   app.useGlobalPipes(
     new ValidationPipe({
@@ -16,5 +20,7 @@ async function bootstrap() {
   );
 
   // await app.listen(process.env.PORT ?? 3000);
+  await app.init();
 }
 bootstrap();
+export const handler = serverless(expressApp);
