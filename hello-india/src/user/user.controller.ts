ba04a09696364020e4a1dcdebbc63958551c5659
YAMITRAO <37434328+YAMITRAO@users.noreply.github.com>
2024-12-28 01:15:11 +0530

    test

--- hello-india/src/user/user.controller.ts
+++ hello-india/src/user/user.controller.ts
@@ -1,22 +1,22 @@
 import {
   Body,
   Controller,
-  Get,
+  // Get,
   Post,
-  Put,
-  Request,
-  UseGuards,
+  // Put,
+  // Request,
+  // UseGuards,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserSignUpDto } from './dto/user-signup.dto';
-import { UserLoginDto } from './dto/user-login.dto';
-import { JwtAuthGuard } from 'src/auth/auth.guard';
-import { Req_Int } from 'src/interface/common';
-import {
-  ChnagePasswordDto,
-  ResetEmailDto,
-  ValidateOtpDto,
-} from './dto/user-password-reset.dto';
+// import { UserLoginDto } from './dto/user-login.dto';
+// import { JwtAuthGuard } from 'src/auth/auth.guard';
+// import { Req_Int } from 'src/interface/common';
+// import {
+//   ChnagePasswordDto,
+//   ResetEmailDto,
+//   ValidateOtpDto,
+// } from './dto/user-password-reset.dto';
 
 @Controller('user')
 export class UserController {
@@ -27,39 +27,39 @@ export class UserController {
     return this.userService.userSignUp(userSignUpDto);
   }
   // to login user
-  @Post('user-login')
-  async userLogin(@Body() userLoginDto: UserLoginDto) {
-    return this.userService.userLogin(userLoginDto);
-  }
+  // @Post('user-login')
+  // async userLogin(@Body() userLoginDto: UserLoginDto) {
+  //   return this.userService.userLogin(userLoginDto);
+  // }
 
   // get-user details
-  @UseGuards(JwtAuthGuard)
-  @Get('get-user')
-  async userGetDetails(@Request() req: Req_Int) {
-    return this.userService.userGetDetails(req.user);
-  }
+  // @UseGuards(JwtAuthGuard)
+  // @Get('get-user')
+  // async userGetDetails(@Request() req: Req_Int) {
+  //   return this.userService.userGetDetails(req.user);
+  // }
 
   // sent otp to reset password
-  @Post('send-otp')
-  userGetResetOtp(@Body() resetEmailDto: ResetEmailDto) {
-    return this.userService.passwordResetOtp(resetEmailDto);
-  }
+  // @Post('send-otp')
+  // userGetResetOtp(@Body() resetEmailDto: ResetEmailDto) {
+  //   return this.userService.passwordResetOtp(resetEmailDto);
+  // }
 
   // validate entered otp
-  @Post('validate-otp')
-  userValidateResetOtp(@Body() validateOtpDto: ValidateOtpDto) {
-    return this.userService.validateResetOtp(validateOtpDto);
-  }
+  // @Post('validate-otp')
+  // userValidateResetOtp(@Body() validateOtpDto: ValidateOtpDto) {
+  //   return this.userService.validateResetOtp(validateOtpDto);
+  // }
 
   // change password
-  @Put('change-password')
-  userChangePassword(@Body() chnagePasswordDto: ChnagePasswordDto) {
-    return this.userService.changeUserPassword(chnagePasswordDto);
-  }
+  // @Put('change-password')
+  // userChangePassword(@Body() chnagePasswordDto: ChnagePasswordDto) {
+  //   return this.userService.changeUserPassword(chnagePasswordDto);
+  // }
 
   // test route
-  @Get('test')
-  testRoute() {
-    return '<div>Test route is working fineeeeee</div>';
-  }
+  // @Get('test')
+  // testRoute() {
+  //   return '<div>Test route is working fineeeeee</div>';
+  // }
 }
