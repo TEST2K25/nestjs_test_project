db183c008259446fe09b0b08642053492a5223da
YAMITRAO <37434328+YAMITRAO@users.noreply.github.com>
2024-12-28 01:08:18 +0530

    serverless problem resolved

--- hello-india/src/app.module.ts
+++ hello-india/src/app.module.ts
@@ -4,14 +4,14 @@ import { AppService } from './app.service';
 import { TestController } from './test/test.controller';
 import { UserModule } from './user/user.module';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ExpenseModule } from './expense/expense.module';
-import { ResourceModule } from './resource/resource.module';
+// import { ExpenseModule } from './expense/expense.module';
+// import { ResourceModule } from './resource/resource.module';
 
 @Module({
   imports: [
     UserModule,
-    ExpenseModule,
-    ResourceModule,
+    // ExpenseModule,
+    // ResourceModule,
     MongooseModule.forRoot(
       'mongodb+srv://rough2k25:c1TZNryekbAnXrvf@onestepinternalsystemdb.yjppo.mongodb.net/?retryWrites=true&w=majority&appName=onestepInternalSystemDb',
     ),
